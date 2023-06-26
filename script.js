259c68cdec5c2ebd7f51794be2b4476b44aad9e9
Dustin-11 <[11kurtzd@gmail.com]>
2023-06-26 17:30:29 -0400

    Finished functionality

--- script.js
+++ script.js
@@ -1,91 +1,39 @@
-const Input = document.getElementById("input");
-Input.textContent = "4";
-const Body = document.getElementById("body");
-Body.style.backgroundColor = "white";
+function main () {
 
-var Rating = {
-    buttonOne: document.getElementById("button__1"),
-    buttonOneValue: 1,
-    buttonTwo: document.getElementById("button__2"),
-    buttonTwoValue: 2,
-    buttonThree: document.getElementById("button__3"),
-    buttonThreeValue: 3,
-    buttonFour: document.getElementById("button__4"),
-    buttonFourValue: 4,
-    buttonFive: document.getElementById("button__5"),
-    buttonFiveValue: 5
+const buttonOne = document.getElementById("button__1").addEventListener("click", buttonOneRating);
+const buttonTwo = document.getElementById("button__2").addEventListener("click", buttonTwoRating);
+const buttonThree = document.getElementById("button__3").addEventListener("click", buttonThreeRating);
+const buttonFour = document.getElementById("button__4").addEventListener("click", buttonFourRating);
+const buttonFive = document.getElementById("button__5").addEventListener("click", buttonFiveRating);
+
+function buttonOneRating() {
+    let value = 1;
+    window.localStorage.setItem('value', JSON.stringify(value));
 }
-function rating (buttonOne, buttonOneValue, buttonTwo, buttonTwoValue, buttonThree, buttonThreeValue, buttonFour, buttonFourValue, buttonFive, buttonFiveValue) {
-    this.buttonOne = buttonOne;
-    this.buttonOneValue = buttonOneValue;
+function buttonTwoRating() {
+    window.localStorage.clear();
+    let value = 2;
+    window.localStorage.setItem('value', JSON.stringify(value));
+    
 }
-const formRating = new rating("buttonOne", 1);
-console.log(Rating);
-Rating.buttonOne.addEventListener("click", test);
-function test() {
-    console.log("Works");
+function buttonThreeRating() {
+    window.localStorage.clear();
+    let value = 3;
+    window.localStorage.setItem('value', JSON.stringify(value));
+    
 }
-   
-  /*  buttonTwo: document.getElementById("button__2"),
-    buttonThree: document.getElementById("button__3"),
-    buttonFour: document.getElementById("button__4"),
-    buttonFive: document.getElementById("button__5"),
+function buttonFourRating() {
+    window.localStorage.clear();
+    let value = 4;
+    window.localStorage.setItem('value', JSON.stringify(value));
+    
 }
-*/
-
-
-
-/*const buttonOne = document.getElementById("button__1");
-const buttonTwo = document.getElementById("button__2");
-const buttonThree = document.getElementById("button__3");
-const buttonFour = document.getElementById("button__4");
-const buttonFive = document.getElementById("button__5");
-const inputResult = document.getElementById("input__result");
-const buttonContainer = document.getElementById("button__container");
-const Submit = document.getElementById("submit");
-//buttonContainer.addEventListener("click", iterateButtons())
-buttonOne.addEventListener("click", addID);
-buttonTwo.addEventListener("click", addID);
-console.log(buttonOne.id, buttonTwo.id, buttonThree.id, buttonFour.id, buttonFive.id);
-
-Submit.addEventListener("click", iterateButtons);
-
-
-function addID() {
-    this.id = "data";
-    console.log(this.id);
-    console.log(buttonOne, buttonOne.id, buttonTwo.id, buttonThree.id, buttonFour.id, buttonFive.id);
-}
-
-function submit() {
-//const test = getElementById("data")
-for (let v = 0; v <= 4; v++) {
-    if (buttonContainer.children[v].id = "data") {
-        alert("This worked");
-    }
+function buttonFiveRating() {
+    window.localStorage.clear();
+    let value = 5;
+    window.localStorage.setItem('value', JSON.stringify(value));
+    
 }
 }
 
-function iterateButtons (){
-    if (buttonOne.id = "data") {
-        alert("Button 1 selected");
-    }
-        else {
-            alert ("Another button was selected");
-        }
-    }
-
-
-  /*  if (buttonTwo.id = "data") {
-        alert("Button 2 selected");
-    }
-    } */
-    /*for (let v = 0; v <= 4; v++) {
-        if (buttonContainer.children[v].class = "test") {
-            alert("This worked");
-        }
-     //   console.log(buttonContainer.children[v].classList);
-    } */
-// console.log(buttonOne.value, buttonTwo.value, buttonContainer.children);
-// inputResult.innerHTML = buttonOne.value;
-// Look into utilizing modules to run program through index.html first then importing to submit.html for functionality
+main();
